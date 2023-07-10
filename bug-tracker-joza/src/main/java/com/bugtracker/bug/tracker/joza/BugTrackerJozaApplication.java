f8ffb21e98be93a1dae2f112802522ce04343c77
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-10 17:48:01 +0200

    Nouveau fichier setup sur https://start.spring.io

---
+++ bug-tracker-joza/src/main/java/com/bugtracker/bug/tracker/joza/BugTrackerJozaApplication.java
@@ -0,0 +1,13 @@
+package com.bugtracker.bug.tracker.joza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerJozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerJozaApplication.class, args);
+	}
+
+}
---
+++ bug-tracker-joza/src/test/java/com/bugtracker/bug/tracker/joza/BugTrackerJozaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugtracker.bug.tracker.joza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerJozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
