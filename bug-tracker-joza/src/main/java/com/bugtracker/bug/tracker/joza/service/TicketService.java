15f84e6b58ad87756d37bb781d56ef53beca22b3
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-12 18:14:03 +0200

    Travail sur le fichier service, création de la class et des différente function public pour le Crud.

--- bug-tracker-joza/src/main/java/com/bugtracker/bug/tracker/joza/service/TicketService.java
+++ bug-tracker-joza/src/main/java/com/bugtracker/bug/tracker/joza/service/TicketService.java
@@ -1,4 +1,57 @@
 package com.bugtracker.bug.tracker.joza.service;
 
-public class TicketService {
+import com.bugtracker.bug.tracker.joza.domain.ticket.Ticket;
+import com.bugtracker.bug.tracker.joza.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TicketService
+{
+    public final TicketService ticketServiceRepository;
+    @Autowired
+    public TicketService(TicketService ticketServiceRepository) {
+        this.ticketServiceRepository = ticketServiceRepository;
+    }
+
+    public Ticket getTicketById(Long id)
+    {
+        return ticketServiceRepository.getTicketById(id);
+    }
+
+    public List<Ticket> getAllTicket(Long id)
+    {
+        return ticketServiceRepository.getAllTicket(id);
+
+    }
+
+    public Ticket createTicket(TicketService ticketService)
+    {
+        return ticketServiceRepository.save(ticketService);
+    }
+
+    private Ticket save(TicketService ticketServiceRepository) {
+        return null;
+    }
+
+    public Ticket updateTicket(Long id, TicketService updatedticketServiceRepository)
+    {
+        TicketService existingTicket = ticketServiceRepository.findById(id).orElse(null);
+        if (existingTicket != null)
+        {
+            existingTicket.setName(updatedticketServiceRepository.getName());
+            existingTicket.setAuthor(updatedticketServiceRepository.getAuthor());
+            existingTicket.setDescription(updatedticketServiceRepository.getDame());
+            existingTicket.setPriority(updatedticketServiceRepository.getPriority());
+
+            return ticketServiceRepository.save(existingTicket);
+        }
+        return null;
+    }
+
+    public Ticket deleteTicket(Long id)
+    {
+        return ticketServiceRepository.deleteById(id);
+    }
 }
