63425625b5f29d4db04229dea7fc8203e0b8ad09
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-28 02:59:39 +0200

    fix scss

--- bug-tracker-joza/react-app/src/pages/Error/404.jsx
+++ bug-tracker-joza/react-app/src/pages/Error/404.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './404.scss';
 
 const NotFound = () => {
     return(
-        <div>
+        <div className="error404">
+            <h1>404</h1>
             <h2>The page you’re looking for doesn’t exist.</h2>
             <button><Link to="/">Go to Home</Link></button>
         </div>
